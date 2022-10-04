f395066e3d44cd64f01538cb26121ea33603b73e
piou <piou@hotmail.co.jp>
2022-10-04 16:19:23 +0900

    Initial commit

--- disable-async-hide.user.js
+++ disable-async-hide.user.js
@@ -2,7 +2,7 @@
 // @name         Disable async-hide
 // @author       piouc
 // @namespace    https://piou.dev
-// @version      2.0.0
+// @version      2.0.1
 // @updateURL    https://raw.githubusercontent.com/piouc/user-scripts/master/disable-async-hide.user.js
 // @downloadURL  https://raw.githubusercontent.com/piouc/user-scripts/master/disable-async-hide.user.js
 // @include      /^https?:\/\/*/
