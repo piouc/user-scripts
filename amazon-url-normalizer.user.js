4b95eb19e1e89fe79226625b8dea375ee311dfed
piou <piou@hotmail.co.jp>
2024-05-02 17:06:46 +0900

    Update

--- amazon-url-normalizer.user.js
+++ amazon-url-normalizer.user.js
@@ -2,62 +2,86 @@
 // @name         Amazon URL normalizer
 // @author       piouc
 // @namespace    https://piou.dev
-// @version      2.1.1
+// @version      3.0.0
 // @updateURL    https://github.com/piouc/user-scripts/raw/main/amazon-url-normalizer.user.js
 // @downloadURL  https://github.com/piouc/user-scripts/raw/main/amazon-url-normalizer.user.js
 // @include      https://www.amazon.co.jp/*
+// @include      https://keepa.com/*
 // @grant        window.onurlchange
 // @run-at       document-start
-// @noframes
 // ==/UserScript==
 
-// Utility functions
-const getId = (url = location.href) => {
-  const regex = /^https:\/\/www.amazon\.co\.jp\/(?:.+\/)?(?:dp|gp\/product)\/([a-zA-Z0-9]+?)(?:[?\/].*|$)/
-  return url.match(regex)?.[1] ?? null
-}
-
-const insertBefore = (el, target) => {
-  if(target){
-    target.parentNode.insertBefore(el, target.nextElementSibling)
+if(location.hostname === 'www.amazon.co.jp'){
+  // Utility functions
+  const getId = (url = location.href) => {
+    const regex = /^https:\/\/www.amazon\.co\.jp\/(?:.+\/)?(?:dp|gp\/product)\/([a-zA-Z0-9]+?)(?:[?\/].*|$)/
+    return url.match(regex)?.[1] ?? null
   }
-}
 
-const parseCookie = str => str.split(';').reduce((acc, record) => {
-  const [key, value] = record.split('=')
-  acc[decodeURIComponent(key.trim())] = decodeURIComponent(value.trim())
-  return acc
-}, {})
+  const insertBefore = (el, target) => {
+    if(target){
+      target.parentNode.insertBefore(el, target.nextElementSibling)
+    }
+  }
 
-// Force language
-if(parseCookie(document.cookie)['lc-acbjp'] !== 'ja_JP'){
-  document.cookie = 'lc-acbjp=ja_JP;path=/;max-age=31536000'
-  location.reload()
-}
+  const parseCookie = str => str.split(';').reduce((acc, record) => {
+    const [key, value] = record.split('=')
+    acc[decodeURIComponent(key.trim())] = decodeURIComponent(value.trim())
+    return acc
+  }, {})
 
-// Add keepa iframe
-const id = getId()
-if(id !== null){
-  // Normalize url
-  history.replaceState(history.state, document.title, `https://www.amazon.co.jp/dp/${id}`)
+  // Force language
+  if(parseCookie(document.cookie)['lc-acbjp'] !== 'ja_JP'){
+    document.cookie = 'lc-acbjp=ja_JP;path=/;max-age=31536000'
+    location.reload()
+  }
 
-  // Insert keepa
-  document.addEventListener('DOMContentLoaded', () => {
-    const iframe = document.createElement('iframe')
-    iframe.style = 'width: 100%; height: 491px; border: 0 none'
-    insertBefore(iframe, document.getElementById('ppd'))
+  // Add keepa iframe
+  const id = getId()
+  if(id !== null){
+    // Normalize url
+    history.replaceState(history.state, document.title, `https://www.amazon.co.jp/dp/${id}`)
 
-    const updateUrl = () => {
-      const id = getId()
-      if(id !== null){
-        iframe.src = `https://keepa.com/iframe_addon.html#5-0-${id}`
+    // Insert keepa
+    document.addEventListener('DOMContentLoaded', () => {
+      const iframe = document.createElement('iframe')
+      iframe.setAttribute('style', 'width: 100%; height: 627px; border: 0 none')
+      insertBefore(iframe, document.getElementById('ppd'))
+
+      const updateUrl = () => {
+        const id = getId()
+        if(id !== null){
+          iframe.src = `https://keepa.com/#!product/5-${id}`
+        }
       }
-    }
 
-    if('onurlchange' in window){
-      window.addEventListener('urlchange', updateUrl)
-    }
+      if('onurlchange' in window){
+        window.addEventListener('urlchange', updateUrl)
+      }
 
-    updateUrl()
-  })
+      updateUrl()
+    })
+  }
+} else {
+  if(top !== window){
+    const style = document.createElement('style')
+    style.innerHTML = `
+      :root {
+        background-color: transparent !important;
+      }
+      body:has(#productTableDescription) {
+        *:not(:has(#productTableDescription)):not(#productTableDescription *, #productTableDescription) {
+          display: none;
+        }
+        *:has(#productTableDescription) {
+          all: unset;
+        }
+      }
+      .productTableDescriptionTitle,
+      .productTableDescriptionBrand {
+        display: none;
+      }
+    `
+    document.head.appendChild(style)
+  }
 }
